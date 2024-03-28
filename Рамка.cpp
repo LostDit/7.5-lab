f4938deef95147ae521f4b213bf52337feaa64ce
Maksim <maksim.antonov2012@yandex.ru>
2024-03-28 15:54:21 +0300

    Чот добавил

--- Рамка.cpp
+++ Рамка.cpp
@@ -1,38 +1,47 @@
-﻿#include <iostream>
-#include <string>
+    #include <iostream>
+    #include <string>
 
-int main()
-{
+int main() {
+    
     setlocale(LC_ALL, "Russian");
     int length, width;
-    std::cout << "Введите высоту и ширину рамки: ";
-    std::cin >> length >> width;
+    
+        std::cout << "Введите высоту и ширину рамки: ";
+        std::cin >> length >> width;
+    
     do{
       
-            if (length == 0 || width == 0)
-            {
-                std::cout<<"Вы ввели 0 попробуйте снова\n";
-                std::cout << "Введите высоту и ширину рамки: ";
-                std::cin >> length >> width;
+        if (length == 0 || width == 0) {
         
+            std::cout<<"Вы ввели 0 попробуйте снова\n";
+            std::cout << "Введите высоту и ширину рамки: ";
+            std::cin >> length >> width;
+        
+        }else if (std::cin,("")) {
+
+            std::cout << "Вы ввели не число";
+            std::cout << "Введите высоту и ширину рамки: ";
+            std::cin >> length >> width;
+
+
         }else {
     
-                std::string mobiusRow = "|", fillerRows = "|";
+            std::string mobiusRow = "|", fillerRows = "|";
     
-                for (int i = 0; i < width - 2; i++)
-                    {
-                        mobiusRow += '-';
-                         fillerRows += ' ';
-                    }
-                mobiusRow += "|\n";
-                fillerRows += "|\n";
-                //std::cout << mobiusRow
-                std::string frame = mobiusRow;
-                for (int i = 0; i < length - 2; i++)
-                    frame += fillerRows;
-                    frame += mobiusRow;
-                    std::cout << frame;
-            }
-    
+        for (int i = 0; i < width - 2; i++) {
+        
+            mobiusRow += '-';
+            fillerRows += ' ';
+        }
+            mobiusRow += "|\n";
+            fillerRows += "|\n";
+            //std::cout << mobiusRow
+            std::string frame = mobiusRow;
+            for (int i = 0; i < length - 2; i++)
+            frame += fillerRows;
+            frame += mobiusRow;
+            std::cout << frame;
+        }
     } while (true);
 }
+//Небольшой допил нужен будет потом
